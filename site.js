82a8260d1e7c1a32782b017f9fd4c2e4847804cd
QingQiz <sofeeys@outlook.com>
2021-09-24 00:36:47 +0800

    selected / full conference publications

--- site.js
+++ site.js
@@ -36,45 +36,67 @@ function insert_paper(paper, selector) {
 
     $.each(paper, function (key, value) {
         if (typeof value === 'string') return;
+        if (value === null || value === undefined) return;
 
-        let linkGroup = document.createElement('span');
-        linkGroup.className = 'link-group';
-        linkGroup.innerText = key;
+        if (value.length === 1 && typeof value[0] === 'string') {
+            let linkGroup = document.createElement('a');
+            linkGroup.className = 'link-group';
+            linkGroup.classList.add('color-red');
+            linkGroup.innerText = key;
+            linkGroup.href = value[0];
+            newLi.appendChild(linkGroup);
+        } else {
+            let linkGroup = document.createElement('span');
+            linkGroup.className = 'link-group';
+            linkGroup.innerText = key;
 
-        for (const i in value) {
-            const datasetLink = value[i];
+            for (const i in value) {
+                const datasetLink = value[i];
 
-            let link = document.createElement('a');
-            link.className = 'link-group-sub';
+                let link = document.createElement('a');
+                link.className = 'link-group-sub';
 
-            if (typeof datasetLink === 'string') {
-                link.href = datasetLink;
-                linkGroup.appendChild(link);
-            } else {
-                link.href = datasetLink[0];
-                linkGroup.appendChild(link);
+                if (typeof datasetLink === 'string') {
+                    link.href = datasetLink;
+                    linkGroup.appendChild(link);
+                } else {
+                    link.href = datasetLink[0];
+                    linkGroup.appendChild(link);
 
-                let accessCode = document.createElement('span');
-                accessCode.className = 'access-code';
-                accessCode.innerText = datasetLink[1];
-                linkGroup.appendChild(accessCode);
+                    let accessCode = document.createElement('span');
+                    accessCode.className = 'access-code';
+                    accessCode.innerText = datasetLink[1];
+                    linkGroup.appendChild(accessCode);
+                }
             }
+            newLi.appendChild(linkGroup);
         }
-        newLi.appendChild(linkGroup);
     });
 
     $(selector).append(newLi);
 }
 
+// selected journal filter
 let journalFilter = function (data) {
     let ret = false;
+    // first author
     if (data['authors'].startsWith('<b')) ret = true;
+    // IEEE Transaction OR ACM Transaction
     if (data['publication'].includes('IEEE Trans') || data['publication'].includes('ACM Trans')) ret = true;
+    // code OR dataset OR demo
     if (data['Code'] !== undefined || data['Dataset'] !== undefined || data['Demo'] !== undefined) ret = true;
     return ret;
 }
 
-$.getJSON('/journal.json', function (data) {
+// selected conference filter
+let conferenceFilter = function (data) {
+    // ICASSP
+    if (!data['publication'].includes('ICASSP')) return true;
+    return false;
+}
+
+// journal publications
+$.getJSON('/paper/journal.json', function (data) {
     let insertData = function (filter=true) {
         $.each(data, function (i, x) {
             if (!filter || journalFilter(x)) {
@@ -85,15 +107,43 @@ $.getJSON('/journal.json', function (data) {
 
     let filter = true;
 
-    $('#full-publications').click(function () {
+    $('#full-j-publications').click(function () {
         $('#journal-publications').empty();
         filter ^= true;
         if (filter) {
-            $('#selected-publications').text('Selected');
-            $('#full-publications').text('Full');
+            $('#selected-j-publications').text('Selected');
+            $('#full-j-publications').text('Full');
         } else {
-            $('#selected-publications').text('Full');
-            $('#full-publications').text('Selected');
+            $('#selected-j-publications').text('Full');
+            $('#full-j-publications').text('Selected');
+        }
+        insertData(filter);
+    });
+
+    insertData(filter);
+});
+
+// conference publications
+$.getJSON('/paper/conference.json', function (data) {
+    let insertData = function (filter=true) {
+        $.each(data, function (i, x) {
+            if (!filter || conferenceFilter(x)) {
+                insert_paper(x, '#conference-publications');
+            }
+        });
+    }
+
+    let filter = true;
+
+    $('#full-c-publications').click(function () {
+        $('#conference-publications').empty();
+        filter ^= true;
+        if (filter) {
+            $('#selected-c-publications').text('Selected');
+            $('#full-c-publications').text('Full');
+        } else {
+            $('#selected-c-publications').text('Full');
+            $('#full-c-publications').text('Selected');
         }
         insertData(filter);
     });
